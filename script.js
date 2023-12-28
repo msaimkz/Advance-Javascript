6017472894cbdb2d27fcbd75c9c6ba80c7058079
Student <LAB-210@aptech-msg.edu>
2023-12-28 12:16:42 +0500

    oop i javascript

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-import { message,name,csl} from "./module.js"
+// **********************************************************************************
+// ****************************Object Oriented Programming in javascript*************
+// **********************************************************************************
+class animals{
 
-//Spread Operator
-let num = [1,2,3,4,5,6]
-let Students = ['Talha','Ali','Anwar','Usman']
+  constructor(character,ability,weigth){
+    this.character=character;
+    this.ability=ability;
+    this.weigth=weigth;
+  }
 
-let MergedArray = [...num,...Students]
-console.log(MergedArray)
-//Spread Operator
-
-// Arrow Function
-var p = document.querySelector("p")
-const add = () => {
-  console.log(p.innerHTML)
-} 
-add();
-// Arrow Function 
-
-// Rest Operator 
-const data = (firstname,lastname,...education) =>{
-console.log(firstname,lastname,...education)
+  walk(){
+    return "walking"
+  }
+  eat(){
+    return"eating"
+  }
 }
-data("Abdullah","Faris","Matric",2022);
-// Rest Operator 
 
-// Task 
-let number = [12,5,15,20,8]
-const sum = (a,b,c,d,e)=>{
-// alert(a+b+c+d+e)
-}
-sum(...number);
-// Task 
+let animal1= new animals("lion","terrific night vision.","190kg")
+let animal2= new animals("elephant","Experts in Affidavits.","4000kg")
 
-// module 
-message();
+console.log(animal2)
 
-console.log(name)
 
-csl();
 
-// module 
+
+
+
+
+
+
+
+
+// **********************************************************************************
+// ****************************Object Oriented Programming in javascript*************
+// **********************************************************************************
 
