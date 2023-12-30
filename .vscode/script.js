57f5d92d469445a6a8f25d362cbd497b6cb8c860
Student <LAB-210@aptech-msg.edu>
2023-12-30 12:40:53 +0500

    APIs

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,4 +1,6 @@
-
+// ********************************************************************************
+// ********************Application Programming Interface***************************
+// ********************************************************************************
 
 
 fetch('https://fakestoreapi.com/products')
@@ -6,6 +8,18 @@ fetch('https://fakestoreapi.com/products')
   return response.json()
 }).then(function(result){
   console.log(result)
+  let container = document.querySelector(".container")
+  for(let index=0;index < result.length; index++){
+  container.innerHTML += `
+  <div class="card" style="width: 18rem;">
+  <img src="${result[index].image}" class="card-img-top" alt="..." width="250px" height="250px">
+  <div class="card-body">
+    <h5 class="card-title">${result[index].title}</h5>
+    <p class="card-text">${result[index].description}</p>
+    <a href="#" class="btn btn-primary">${result[index].price}</a>
+  </div>
+  </div>`
+  }
 })
 
 
@@ -14,5 +28,24 @@ fetch('https://dummyjson.com/products')
 .then(function(response){
   return response.json()
 }).then(function(result){
-  console.log(result)
+  let container = document.querySelector(".container")
+for(let index=0;index < result.products.length; index++){
+container.innerHTML += `
+<div class="card" style="width: 18rem;">
+<img src="${result.products[index].images[0]}" class="card-img-top" alt="..." width="250px" height="250px">
+<div class="card-body">
+  <h5 class="card-title">${result.products[index].title}</h5>
+  <p class="card-text">${result.products[index].description}</p>
+  <a href="#" class="btn btn-primary">${result.products[index].price}</a>
+</div>
+</div>`
+}
+console.log(result.products)
+
 })
+
+
+
+// ********************************************************************************
+// ********************Application Programming Interface***************************
+// ********************************************************************************
