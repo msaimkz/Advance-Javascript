c8cf95c8aca99d88f797c9904860816fd206bbe0
Student <LAB-210@aptech-msg.edu>
2024-01-06 11:43:52 +0500

    Crud

--- script.js
+++
@@ -1,9 +0,0 @@
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=6c8daf0357046c83abd77d5729679271"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (result) {
-    console.log(result);
-  });
