f90780255c60cef042086db7262e96d8ee480e38
Student <LAB-210@aptech-msg.edu>
2024-01-09 12:09:22 +0500

    Crud

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ let input=document.querySelector("#add")
 document.querySelector(".add").addEventListener('click',function(){
   document.querySelector(".list").innerHTML+=`
   <div id="item">
-  <p>${input.value}</p> <button onclick="del(this)">Delete</button> <button onclick="edit()">Edit</button>
+  <p>${input.value}</p> <div><button onclick="del(this)">Delete</button> <button onclick="edit(this)">Edit</button></div>
 </div>
   `
+  input.value=''
+  
+
 })
+let para=document.querySelector(".item p")
 function del(a){
-  a.parentElement.remove()
+  a.parentElement.parentElement.remove()
 }
-function edit(a){
-  a.parentElement.remove()
+input.setAttribute
+function edit(data){
+  data.parentElement.parentElement.children[0].setAttribute("contenteditable","true")
 }
